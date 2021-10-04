de9e7486c3965ecaea910567d376d780c9b3bfd3
BAEdwardss <91565774+BAEdwardss@users.noreply.github.com>
2021-10-04 15:02:47 -0500

    Add files via upload
    Started working on colors project

---
+++ RandomColors.py
@@ -0,0 +1,17 @@
+#Import
+
+import turtle
+import random
+
+Beck=turtle.Turtle()
+
+#Functions
+def randomDot():
+    Beck.color("red")
+    Beck.begin_fill()
+    Beck.circle(100)
+    Beck.end_fill()
+
+#Main
+randomDot()
+    
