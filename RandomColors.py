21de2382ca4ee4b9eaf75137bf880f025a4789ce
BAEdwardss <91565774+BAEdwardss@users.noreply.github.com>
2021-10-08 14:07:26 -0500

    Update RandomColors.py

--- RandomColors.py
+++ RandomColors.py
@@ -1,17 +1,37 @@
-#Import
+##Initialize
 
 import turtle
 import random
+t=turtle.Turtle()
+t.speed(9999)
+turtle.colormode(255)
+r=random.randint(0,255)
+g=random.randint(0,255)
+b=random.randint(0,255)
+size=random.randint(10,150)
+xran=random.randint(-500,500)
+yran=random.randint(-500,500)
 
-Beck=turtle.Turtle()
+##Function
 
-#Functions
 def randomDot():
-    Beck.color("red")
-    Beck.begin_fill()
-    Beck.circle(100)
-    Beck.end_fill()
+    t.color(r, g, b)
+    t.begin_fill()
+    t.circle(size)
+    t.end_fill()
 
-#Main
-randomDot()
+##Main
+    
+for i in range(100):
+    t.penup()
+    t.goto(xran, yran)
+    t.pendown()
+    randomDot()
+    r=random.randint(0,255)
+    g=random.randint(0,255)
+    b=random.randint(0,255)
+    size=random.randint(10,150)
+    xran=random.randint(-500,500)
+    yran=random.randint(-500,500)
+
     
